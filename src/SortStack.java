3855a9fbb81e778b5789a078bfd38da94f0e1c59
rashmibtp <42645752+rashmibtp@users.noreply.github.com>
2024-04-17 21:10:34 -0700

    Update SortStack.java

--- src/SortStack.java
+++ src/SortStack.java
@@ -2,18 +2,18 @@
 // Use this editor to write, compile and run your Java code online
 import java.io.*;
 import java.util.*;
-class HelloWorld {
+class SortStack {
     public static void main(String[] args) {
         System.out.println("Try programiz.pro");
         Stack<Integer> st = new Stack<Integer>();
         st.push(8);st.push(7);st.push(4);st.push(6);
         st.push(5);st.push(2);st.push(3);st.push(1);
         System.out.println(Arrays.toString(st.toArray()));
-        sortStack(st);
+        sortStack1(st);
         System.out.println(Arrays.toString(st.toArray()));
     }
     
-    public static void sortStack(Stack<Integer> st){
+    public static void sortStack1(Stack<Integer> st){
         if(st.isEmpty()) return;
         int curr = st.pop();
         //System.out.print(curr+", ");
@@ -26,14 +26,14 @@ class HelloWorld {
         }
         //System.out.print(curr+", ");
         //System.out.println(Arrays.toString(st.toArray()));
-        sortStack(st);
+        sortStack1(st);
         //if(st.isEmpty()) return;
         if(st.peek()>curr){
             //replace
             int temp = st.pop();
             st.push(curr);
             curr=temp;
-            sortStack(st);
+            sortStack1(st);
         }
         st.push(curr);
         //System.out.println(Arrays.toString(st.toArray()));
